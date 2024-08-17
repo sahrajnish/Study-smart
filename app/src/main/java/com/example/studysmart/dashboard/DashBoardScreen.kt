a270a4c4fb08f7ba70c3f9d7eea340fa05463eb9
rajnishsah <raj.rajnish.sah@gmail.com>
2024-08-17 21:50:32 +0530

    DashBoardScreen

--- app/src/main/java/com/example/studysmart/dashboard/DashBoardScreen.kt
+++ app/src/main/java/com/example/studysmart/dashboard/DashBoardScreen.kt
@@ -1,24 +1,53 @@
 package com.example.studysmart.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.studysmart.R
 import com.example.studysmart.components.CountCard
+import com.example.studysmart.components.SubjectCard
+import com.example.studysmart.domain.model.Subject
+import javax.security.auth.SubjectDomainCombiner
 
 @Composable
 fun DashBoardScreen(modifier: Modifier = Modifier) {
+
+    val subjects = listOf(
+        Subject(name = "English", goalHours = 10f, colors = Subject.subjectCardColors[0]),
+        Subject(name = "Maths", goalHours = 10f, colors = Subject.subjectCardColors[1]),
+        Subject(name = "Physics", goalHours = 10f, colors = Subject.subjectCardColors[2]),
+        Subject(name = "DSA", goalHours = 10f, colors = Subject.subjectCardColors[3]),
+        Subject(name = "Biology", goalHours = 10f, colors = Subject.subjectCardColors[4]),
+    )
+
     Scaffold(
         topBar = { Title() }
     ) { paddingValues ->
@@ -35,6 +64,12 @@ fun DashBoardScreen(modifier: Modifier = Modifier) {
                     subjectCount = 3,
                     studiedHour = "10",
                     goalHour = "15"
+                )
+            }
+            item {
+                SubjectCardsSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectList = subjects
                 )
             }
         }
@@ -82,8 +117,66 @@ private fun CountCardSection(
     }
 }
 
-@Preview
 @Composable
-private fun prev() {
+private fun SubjectCardsSection(
+    modifier: Modifier = Modifier,
+    subjectList: List<Subject>,
+    emptyListText: String = "You don't have any subjects. \n Click + to add new subject."
+) {
+    Column (
+        modifier = modifier
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "SUBJECTS",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add Subjects",
+                modifier = Modifier.padding(end = 12.dp)
+            )
+        }
+        if(subjectList.isEmpty()) {
+            Image(
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(id = R.drawable.img_books),
+                contentDescription = emptyListText
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = emptyListText,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(start = 12.dp, end = 12.dp)
+        ) {
+            items(subjectList) { subject ->
+                SubjectCard(
+                    subjectName = subject.name,
+                    gradientColor = subject.colors,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun prev() {
     DashBoardScreen()
 }
