0042b41e8ee1d0d91344de867d2604c6e27f203f
rajnishsah <raj.rajnish.sah@gmail.com>
2024-08-23 22:18:30 +0530

    DialogBoxDesign

--- app/src/main/java/com/example/studysmart/dashboard/DashBoardScreen.kt
+++ app/src/main/java/com/example/studysmart/dashboard/DashBoardScreen.kt
@@ -21,10 +21,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -34,6 +39,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.studysmart.R
+import com.example.studysmart.components.AddSubjectDialog
 import com.example.studysmart.components.CountCard
 import com.example.studysmart.components.SubjectCard
 import com.example.studysmart.components.studySessionList
@@ -138,6 +144,14 @@ fun DashBoardScreen(modifier: Modifier = Modifier) {
         )
     )
 
+    var isAddSubjectDialogOpen by rememberSaveable { mutableStateOf(false) }
+
+    AddSubjectDialog(
+        isOpen = isAddSubjectDialogOpen,
+        onDismissRequest = { isAddSubjectDialogOpen = false },
+        onConfirmButtonClick = { isAddSubjectDialogOpen = false }
+    )
+
     Scaffold(
         topBar = { Title() }
     ) { paddingValues ->
@@ -159,7 +173,8 @@ fun DashBoardScreen(modifier: Modifier = Modifier) {
             item {
                 SubjectCardsSection(
                     modifier = Modifier.fillMaxWidth(),
-                    subjectList = subjects
+                    subjectList = subjects,
+                    onAddIconClick = { isAddSubjectDialogOpen = true }
                 )
             }
             item {
@@ -242,7 +257,8 @@ private fun CountCardSection(
 private fun SubjectCardsSection(
     modifier: Modifier = Modifier,
     subjectList: List<Subject>,
-    emptyListText: String = "You don't have any subjects. \n Click + to add new subject."
+    emptyListText: String = "You don't have any subjects. \n Click + to add new subject.",
+    onAddIconClick: () -> Unit
 ) {
     Column (
         modifier = modifier
@@ -259,11 +275,12 @@ private fun SubjectCardsSection(
                 style = MaterialTheme.typography.bodySmall,
                 modifier = Modifier.padding(start = 12.dp)
             )
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Add Subjects",
-                modifier = Modifier.padding(end = 12.dp)
-            )
+            IconButton(onClick = onAddIconClick) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Subjects"
+                )
+            }
         }
         if(subjectList.isEmpty()) {
             Image(
